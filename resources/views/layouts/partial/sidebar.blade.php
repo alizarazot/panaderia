1d72e7bf0004bdabc59faa051660ddbfc7da67ad
alizarazot <alizarazot@ufpso.edu.co>
2024-03-06 17:43:18 -0500

    Fix things

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -6,20 +6,8 @@
     <span class="brand-text font-weight-light">AdminLTE 3</span>
   </a>
 
-  <!-- Sidebar -->
-  <div class="sidebar">
-    <!-- Sidebar user panel (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-        <img src="{{asset('backend/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-      </div>
-      <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
-      </div>
-    </div>
-
     <!-- SidebarSearch Form -->
-    <div class="form-inline">
+    <div class="">
       <div class="input-group" data-widget="sidebar-search">
         <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
         <div class="input-group-append">
