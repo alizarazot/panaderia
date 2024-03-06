8cb21f7446462c5540f687c3965a3f1185bf6080
alizarazot <alizarazot@ufpso.edu.co>
2024-03-06 16:46:18 -0500

    Add meta edge

--- resources/views/layouts/applogin.blade.php
+++ resources/views/layouts/applogin.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <!-- CSRF Token -->
