436eac90f98f0534bfa95eb46d3769ddfb63cd7e
alizarazot <alizarazot@ufpso.edu.co>
2024-03-06 17:14:06 -0500

    Style home blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,10 +41,12 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     @yield('content')
 
+
+
 <!-- jQuery -->
-<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('backend/plugins/jquery/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{asset('backend/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
