8e04503fe6f13fa1b43205abc94fe6e9f869f13b
alizarazot <alizarazot@ufpso.edu.co>
2024-03-14 20:28:00 +0100

    Style password reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.applogin')
 
 @section('content')
-<div class="container">
+<div class="container login-box">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,7 +19,6 @@
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
