66ba0005aa348a0b3534280df4141abb4f1e5387
alizarazot <alizarazot@ufpso.edu.co>
2024-03-14 20:19:40 +0100

    Delete annoying pop up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,8 +75,6 @@
     <script src="{{ asset('backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('backend/dist/js/adminlte.js') }}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('backend/dist/js/demo.js') }}"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="{{ asset('backend/dist/js/pages/dashboard.js') }}"></script>
 
