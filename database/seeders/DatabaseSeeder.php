4fc5ef07ea87860ba6984bcb998bca9117175e22
alizarazot <alizarazot@ufpso.edu.co>
2024-03-21 15:22:50 -0500

    Fix seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\Product;
+use App\Models\Provider;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Client::factory(5)->create();
+        Provider::factory(5)->create();
+        Product::factory(15)->create();
+        Order::factory(7)->create();
+        OrderDetail::factory(7)->create();
     }
 }
